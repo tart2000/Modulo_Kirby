b58dcd4732e4238d9f630e71495378071e7d2079
ancrenoire <julien.debyser@gmail.com>
2024-03-22 14:39:24 +0100

    feat Mautic script

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,44 +1,57 @@
 <!doctype html>
 <html lang="<?= $kirby->language()->code() ?>">
 
-  <head>
-    <meta charset="utf-8">
-    <title><?= $site->title() ?> | <?= $site->baseline() ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="description" content="Modulo permet de construire facilement le déroulé de son événement, de le partager, et de suivre son événement en temps réel."/>
-    <meta name="robots" content="index, follow"/>
-    <meta name="Author" content="Arthur Schmitt"/> 
+<head>
+  <meta charset="utf-8">
+  <title><?= $site->title() ?> | <?= $site->baseline() ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Modulo permet de construire facilement le déroulé de son événement, de le partager, et de suivre son événement en temps réel." />
+  <meta name="robots" content="index, follow" />
+  <meta name="Author" content="Arthur Schmitt" />
 
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet" />
 
-    <?= css([
-      'assets/css/socicon.css',
-      'assets/css/entypo.css',
-      'assets/css/theme.css',
-      'assets/css/custom.css',
-      '@auto'
-    ]) ?>
+  <?= css([
+    'assets/css/socicon.css',
+    'assets/css/entypo.css',
+    'assets/css/theme.css',
+    'assets/css/custom.css',
+    '@auto'
+  ]) ?>
 
-    <!-- favicons --> 
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= kirby()->urls()->assets() ?>/favicons/apple-touch-icon.png"/>
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= kirby()->urls()->assets() ?>/favicons/favicon-32x32.png"/>
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= kirby()->urls()->assets() ?>/favicons/favicon-16x16.png"/>
+  <!-- favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= kirby()->urls()->assets() ?>/favicons/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= kirby()->urls()->assets() ?>/favicons/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= kirby()->urls()->assets() ?>/favicons/favicon-16x16.png" />
 
-    <!-- Share stuff --> 
-    <meta property="og:title" content="le créateur de déroulés pour les facilitateurs"/>
-    <meta property="og:site_name" content="Modulo"/>
-    <meta property="og:url" content="http://modulo.io"/>
-    <meta property="og:description" content="Modulo permet de construire facilement le déroulé de son événement, de le partager, et de suivre son événement en temps réel."/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:image" content="<?= kirby()->urls()->assets() ?>/img/share_image_modulo.png"/>
+  <!-- Share stuff -->
+  <meta property="og:title" content="le créateur de déroulés pour les facilitateurs" />
+  <meta property="og:site_name" content="Modulo" />
+  <meta property="og:url" content="http://modulo.io" />
+  <meta property="og:description" content="Modulo permet de construire facilement le déroulé de son événement, de le partager, et de suivre son événement en temps réel." />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="<?= kirby()->urls()->assets() ?>/img/share_image_modulo.png" />
 
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image"/>
-    <meta property="twitter:url" content="http://modulo.io/"/>
-    <meta property="twitter:title" content="Modulo | le créateur de déroulés pour les facilitateurs"/>
-    <meta property="twitter:description" content="Modulo permet de construire facilement le déroulé de son événement, de le partager, et de suivre son événement en temps réel."/>
-    <meta property="twitter:image" content="<?= kirby()->urls()->assets() ?>/img/share_image_modulo.png"/>
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="http://modulo.io/" />
+  <meta property="twitter:title" content="Modulo | le créateur de déroulés pour les facilitateurs" />
+  <meta property="twitter:description" content="Modulo permet de construire facilement le déroulé de son événement, de le partager, et de suivre son événement en temps réel." />
+  <meta property="twitter:image" content="<?= kirby()->urls()->assets() ?>/img/share_image_modulo.png" />
+  <script>
+    (function(w, d, t, u, n, a, m) {
+      w['MauticTrackingObject'] = n;
+      w[n] = w[n] || function() {
+          (w[n].q = w[n].q || []).push(arguments)
+        }, a = d.createElement(t),
+        m = d.getElementsByTagName(t)[0];
+      a.async = 1;
+      a.src = u;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://go.modulo.io/mtc.js', 'mt');
 
-  </head>
+    mt('send', 'pageview');
+  </script>
+</head>
 
-  <body>
+<body>
